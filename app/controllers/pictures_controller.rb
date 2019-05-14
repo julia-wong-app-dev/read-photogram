69f972629391d5f34c454cad6d32271c6b4f5aa3
Julia Wong <jwong8@chicagobooth.edu>
2019-05-14 14:34:12 +0000

    recent

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,7 @@
 class PicturesController < ApplicationController
     
     def latest
-        
+        @photos = Photo.order({ :created_at => :desc }).limit(25)
         render("pic_templates/recent.html.erb")
         
     end
